1ffbc3fce194575baf15b9792aa3c9457fb11c5e
andferrario <andrea.ferrario92@gmail.com>
2021-03-05 10:19:07 +0100

    Add custom Button UI component

---
+++ src/UI/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/Button.css';
+
+interface ButtonProps {
+  clicked: () => void;
+  buttonType: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({ children, clicked, buttonType }) => (
+  <button onClick={clicked} className={['button', buttonType].join(' ')}>
+    {children}
+  </button>
+);
