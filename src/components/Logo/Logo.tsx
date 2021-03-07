74de8c5ab07e8233ba2dd024fafbdff5b1ef1e4d
andferrario <andrea.ferrario92@gmail.com>
2021-03-07 17:40:29 +0100

    added logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import burgerLogo from '../../assets/images/burger-logo.png';
+import '../../styles/Logo.css';
 
 export const Logo: React.FC = () => (
-  <div>
+  <div className="logo">
     <img src={burgerLogo} alt="burger logo" />
   </div>
 );
